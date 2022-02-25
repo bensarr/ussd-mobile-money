94c5a00221a1256635dc41a71d390805368172cf
bensarr <bensarr37@gamil.com>
2022-02-25 02:36:09 +0000

    initial commit for ussd application

---
+++ util.php
@@ -0,0 +1,27 @@
+<?php
+class Util{
+    static $GO_BACK = "98";
+
+    static $GO_TO_MAIN_MENU = "99";
+
+    static  $API_KEY = "8b2d155d522138ce2f1ee5762158c24693dc7378fdf79b201c7928235edc3aa7"; //sandbox
+
+    static $API_USERNAME = "sandbox"; //sandbox
+
+    static $COMPANY_NAME = "<SENDER ID here>";
+
+    static $COUNTRY_CODE = "YOu country code here e.g. +254";
+
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
